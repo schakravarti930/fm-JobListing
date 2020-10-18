81d635b8e730a73d9346b89d06ca57b307383239
Swapnil <schakravarti930@gmail.com>
2020-10-18 16:44:10 +0530

    initial commit

---
+++ job-listing/src/Card.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import data from './data.json';
+
+const Card = () =>{
+    return(
+        <div className = "Main">
+            <div className = "Content">
+               <img src = "./account.svg" alt = "Hello"></img>
+            </div>
+        </div>
+    );
+}
+export default Card;
